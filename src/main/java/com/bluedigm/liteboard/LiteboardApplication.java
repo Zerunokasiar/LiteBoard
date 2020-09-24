53753726100650da99b933e88a36cc0b4bc2f1a2
Zerunokasiar <woopower94@naver.com>
2020-09-24 13:13:20 +0900

    java 1.8

---
+++ src/main/java/com/bluedigm/liteboard/LiteboardApplication.java
@@ -0,0 +1,13 @@
+package com.bluedigm.liteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluedigm/liteboard/LiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedigm.liteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
